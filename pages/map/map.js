1b69e4af4a76ee2b56d0c7259e478b941677e1d4
liy010 <1056094880@qq.com>
2018-10-15 13:01:25 +0800

    10-15

--- pages/map/map.js
+++ pages/map/map.js
@@ -12,7 +12,10 @@ Page({
       latitude: null,
       longitude: null,
       name: 'Home'
-    }]
+    }],
+    x: 0,
+    y: 0,
+    hidden: true
   },
 
   /**
@@ -61,6 +64,46 @@ Page({
           }
         })
       }
+    })
+    const ctx1 = wx.createCanvasContext('myCanvas1');
+    const ctx2 = wx.createCanvasContext('myCanvas2');
+    const ctx3 = wx.createCanvasContext('myCanvas3');
+    const ctx4 = wx.createCanvasContext('myCanvas4');
+    ctx1.setFillStyle('red')
+    ctx1.fillRect(80, 40, 150, 75)
+    ctx1.draw()
+
+    const grd = ctx3.createLinearGradient(0, 0, 200, 0)
+    grd.addColorStop(0, 'black')
+    grd.addColorStop(0.5, 'blue')
+    grd.addColorStop(1, 'white')
+    ctx3.setFillStyle(grd)
+    ctx3.fillRect(80, 40, 150, 75)
+    ctx3.draw()
+
+    const cgrd = ctx4.createCircularGradient(145, 60, 50)
+    cgrd.addColorStop(0, 'black')
+    cgrd.addColorStop(1, 'white')
+    ctx4.setFillStyle(cgrd)
+    ctx4.fillRect(80, 40, 150, 100)
+    ctx4.draw()
+  },
+  start(e) {
+    this.setData({
+      hidden: false,
+      x: e.touches[0].x,
+      x: e.touches[0].y
+    })
+  },
+  move(e) {
+    this.setData({
+      x: e.touches[0].x,
+      y: e.touches[0].y
+    })
+  },
+  end(e) {
+    this.setData({
+      hidden: true
     })
   },
   markertap(e) {
