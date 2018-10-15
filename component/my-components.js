8102ae896f9c0ac7b18804970c0db7b3079e7c87
liy010 <1056094880@qq.com>
2018-10-15 11:05:20 +0800

    10-15
    10-15

---
+++ component/my-components.js
@@ -0,0 +1,26 @@
+// component/my-components.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    innerText: {
+      type: String,
+      value: 'ggggg'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
